4f8252c88aec69039cc70b332934891ea14d95fa
Mahmoud ElNeweshy <neweshy@Mahmouds-MacBook-Pro.local>
2022-01-08 18:25:41 +0200

    striping strings in the results

--- analyser.py
+++ analyser.py
@@ -26,6 +26,7 @@ def analyse_lines(text_file_lines):
     time = None
     action = False
     message = None
+    action_type = None
 
     data = []
 
@@ -39,6 +40,8 @@ def analyse_lines(text_file_lines):
         # In case of empty line, ignore line and remove 'new line' from other lines
         if line == '\n':
             continue
+        elif 'Messages and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them.' in line:
+            continue
         else:
             line = line.replace('\n', '')
 
@@ -47,14 +50,28 @@ def analyse_lines(text_file_lines):
             line = line.replace(date_time[0], '')
             line = line.replace(action_symbol[0], '')
             try:
-                split = re.split(':', line)
-                sender = split[0]
-                message = split[1]
+                if ':' in line:
+                    split = re.split(':', line)
+                    sender = split[0]
+                    action_type = split[1]
+                elif 'created' in line:
+                    split = re.split('created', line)
+                    sender = split[0]
+                    action_type = 'created' + split[1]
+                elif 'changed' in line:
+                    split = re.split('changed', line)
+                    sender = split[0]
+                    action_type = 'changed' + split[1]
+                elif 'added' in line:
+                    split = re.split('added', line)
+                    sender = split[0]
+                    action_type = 'added' + split[1]
+                
+                message = None
                 date = dates[0]
                 time = times[0]
-                action = True
             except:
-                message = line
+                action_type = line
 
         # 2- In case a new message has started
         elif len(date_time) != 0:
@@ -65,27 +82,33 @@ def analyse_lines(text_file_lines):
                 message = split[1]
                 date = dates[0]
                 time = times[0]
-                action = False
+                action_type = None
             except:
                 message = line
 
         # 3- In case of contuniong talk
         else:
             message = line
-            action = False
+            action_type = None
 
+        # Appending data
         info = {'date': date, 'time': time, 'sender': sender,
-                'message': message, 'action': action}
+                'message': message, 'action type': action_type}
         data.append(info)
+    
     return data
 
 
 def create_csv(lst):
-    df = pd.DataFrame(columns=['date', 'time', 'sender', 'message', 'action'])
+    df = pd.DataFrame(columns=['date', 'time', 'sender', 'message', 'action type'])
 
     for dict in lst:
         df = df.append(dict, ignore_index=True)
 
+    # strip all spaces
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+    # Write data in a csv
     df.to_csv('results.csv', sep=',', encoding='utf-8-sig')
 
     return df
@@ -95,4 +118,3 @@ if __name__ == '__main__':
     text_file_lines = read_txt_file('_chat.txt')
     data = analyse_lines(text_file_lines)
     create_csv(data)
-    # print(data[0])
